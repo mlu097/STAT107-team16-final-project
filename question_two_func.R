c6e01a7ee214d6831e33fba4881314c7c741f5c7
mrchiko31 <mrchiko31@gmail.com>
2025-11-07 22:01:44 -0800

    Create question_two_func.R

---
+++ question_two_func.R
@@ -0,0 +1,3 @@
+## Question 2 is "How does the proportion of overlap (ideological overlap between parties) in Congress change around periods of large shifts in primary dynamics (e.g. big wave years)?"
+
+## For this, we're attempting to connect 2 things over time so we can use primary dynasim 
